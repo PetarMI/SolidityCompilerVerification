ca24851ae5ed71178c72579eee1fc5d11611e70d
PetarMI <petar2394@gmail.com>
2018-04-28 17:22:14 +0200

    Clean up tests from generator since now they are in a separate file.

--- tautology_generator.py
+++ tautology_generator.py
@@ -27,6 +27,7 @@ def gen_tautology():
 def gen_expr(bvalue, depth):
     expr = ""
 
+    # we reached the max depth - return a leaf
     if(depth < 1 or decision(0.9)):
         return str(bvalue)
 
@@ -61,40 +62,6 @@ def gen_false(depth):
 def decision(prob):
     """generate something wih a certain probability"""
     return (random.random() > prob)
-
-def do_tests():
-    true_pass = True
-    false_pass = True
-    num_expr = 500
-    depth = 10
-
-    print("Testing true statements")
-    for i in range(num_expr):
-        expr = gen_expr(True, depth)
-        res = eval(expr)
-
-        if(res == False):
-            print ("Error in True at {0}".format(i))
-            true_pass = False
-
-    if(true_pass):
-        print ("All true tests passed")
-    else: 
-        print ("True tests NOT passed")
-
-    print("Testing true statements")
-    for i in range(num_expr):
-        expr = gen_expr(False, depth)
-        res = eval(expr)
-
-        if(res != False):
-            print ("Error in False at {0}".format(i))
-            false_pass = False
-
-    if(false_pass):
-        print ("All false tests passed")
-    else: 
-        print ("False tests NOT passed")
 
 def run_generator():
     #do_tests()
