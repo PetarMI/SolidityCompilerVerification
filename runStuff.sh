e749ccd533e8946de0a8ef2dc1e81590ccff3a2d
Ali Balkis <alibalkis94@gmail.com>
2018-05-02 14:52:41 +0200

    small thing

--- runStuff.sh
+++ runStuff.sh
@@ -4,7 +4,8 @@ truffle compile
 echo =========================================
 echo Compilation complete, copying files...
 cp /home/ali/Documents/MSc1/ProgAnalysis/Project/projTruffle/contracts/* /home/ali/GitHub/SolidityCompilerVerification/contracts
-cp /home/ali/Documents/MSc1/ProgAnalysis/Project/projTruffle/build/contracts/* /home/ali/GitHub/SolidityCompilerVerification/contractsecho =========================================
+cp /home/ali/Documents/MSc1/ProgAnalysis/Project/projTruffle/build/contracts/* /home/ali/GitHub/SolidityCompilerVerification/contracts
+echo =========================================
 echo Running runner.py
 cd /home/ali/GitHub/SolidityCompilerVerification/
 python3 runner.py
