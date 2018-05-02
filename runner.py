287f0da8fe62586b62a4b71a0651aa69c9412a31
Ali Balkis <alibalkis94@gmail.com>
2018-05-02 17:01:55 +0200

    use vars & mixed and/ors

--- runner.py
+++ runner.py
@@ -15,7 +15,11 @@ ast_walker.pretty_print_vars(vars)
 # mutator.do_mutation(contract_file, if_sources)
 
 print("\nRunning inequality generator. OUTPUT:")
-#ineq_gen.run_generator()
+in_eq,var_eq = ineq_gen.run_generator(vars)
+print(in_eq)
+print("=======================================================================================================================================")
+print("With vars:")
+print(var_eq)
 
 print("\nRunning boolean generator. OUTPUT:")
 tautology_generator.run_generator(vars)
