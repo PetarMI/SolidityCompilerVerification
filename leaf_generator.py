e28624b69e183b4a7dacd4405fe1d1f42023b3ec
Ali Balkis <alibalkis94@gmail.com>
2018-05-16 16:01:21 +0200

    imma gonna have more commits than you

--- leaf_generator.py
+++ leaf_generator.py
@@ -1,4 +1,5 @@
 import random
+import ineq_gen
 
 all_leaves = { "bool" : { True : ["{0} || true", "true || {0}"],
                           False : ["{0} && false", "false && {0}"] } }
@@ -6,6 +7,8 @@ all_leaves = { "bool" : { True : ["{0} || true", "true || {0}"],
 def get_leaf(leaf_T, bvalue):
     leaves = all_leaves.get(leaf_T, None)
 
+    if leaf_T == "uint" or leaf_T == "int":
+        leaves = ineq_gen.run_generator(True, bvalue)
     # sanity check
     if (not leaves):
         raise ValueError("No leaves of the following type")
